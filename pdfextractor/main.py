4ab3c15b7d5a640c3222ad6377baa029b9389904
Michal Trzcinka <michal@trzcinka.pl>
2025-08-21 11:38:27 +0200

    Add ollama calls

--- pdfextractor/main.py
+++ pdfextractor/main.py
@@ -1,10 +1,11 @@
 import pathlib
 
 import pymupdf4llm
+from ai import extract_customer_name
 
-print("Hello World")
 for pdf_file in pathlib.Path("data").iterdir():
     if pdf_file.is_file():
+        print("Processing file:", pdf_file)
         md_text = pymupdf4llm.to_markdown(str(pdf_file))
-        output_file = pathlib.Path("output_" + pdf_file.stem + ".md")
-        output_file.write_bytes(md_text.encode())
+        customer_name = extract_customer_name(md_text)
+        print("Customer name:", customer_name)
